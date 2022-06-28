a13effd084ecf7d36f3beb62e6ab79ad50377db2
Liliana <liliana.m.parra@gmail.com>
2022-06-28 17:53:08 -0400

    Created endpoint to DELETE card

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -6,3 +6,12 @@ from app.helper_functions import *
 card_bp = Blueprint('Cards', __name__, url_prefix='/cards')
 
 
+
+#deleting one card
+@card_bp.route("/<card_id>", methods=["DELETE"])
+def get_one_board(card_id):
+    card = get_record_by_id(Card, card_id)
+    db.session.delete(card)
+    db.session.commit()
+    
+    return success_message_info_as_list(dict(details=f'Card {card.card_id} "{card.message}" successfully deleted'))
