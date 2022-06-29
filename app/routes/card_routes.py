a81af8a231ddaf1b7c40c8b09af85b49b3ecd31f
Esther Annorzie <estherannorzie@gmail.com>
2022-06-29 12:56:57 -0400

    Began post route for creating a card

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -15,3 +15,16 @@ def get_one_board(card_id):
     db.session.commit()
     
     return success_message_info_as_list(dict(details=f'Card {card.card_id} "{card.message}" successfully deleted'))
+
+
+# creating one card
+@card_bp.route("", methods=["POST"])
+def create_card():
+    request_body = request.get_json()
+    if "message" not in request_body:
+        error_message("Message not found", 400)
+    card = Card.self_to_dict(request_body)
+    db.session.add(card)
+    db.session.commit()
+    
+    return success_message_info_as_list("Card created successfully", status_code=200)
