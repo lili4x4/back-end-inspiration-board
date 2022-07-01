0a55ffbc6915c3b7c03ab758d2ef6c81a946ea61
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-06-30 21:29:49 -0400

    removed unecessary fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,48 +32,13 @@ def one_board_no_cards(app):
     db.session.add(new_board)
     db.session.commit()
 
-# This fixture creates one board with two cards and saves it in the database
-@pytest.fixture
-def one_board_two_cards(app):
-    card_1 = {
-            "board_id": 1,
-            "card_id": 1,
-            "likes_count": 0,
-            "message": "The woods are lovely, dark and deep..."
-        }
-    card_2 = {
-            "board_id": 1,
-            "card_id": 2,
-            "likes_count": 0,
-            "message": "Las ramas de los árboles están envueltas en fundas de hielo."
-        }
-    new_board = Board(
-        title="Winter", 
-        owner= "Lili",
-        board_id=1,
-        cards=[{
-            "board_id": 1,
-            "card_id": 1,
-            "likes_count": 0,
-            "message": "The woods are lovely, dark and deep..."
-        },{
-            "board_id": 1,
-            "card_id": 2,
-            "likes_count": 0,
-            "message": "Las ramas de los árboles están envueltas en fundas de hielo."
-        }]
-    )
-    db.session.add(new_board)
-    db.session.commit()
-
-
-    client.post("/boards/1/cards", request_body_1)
-    client.post("/boards/1/cards", request_body_2)
-
 # This fixture creates two boards with no cards and saves them in the database
 @pytest.fixture
-def two_boards_no_cards(app, one_board_no_cards):
+def two_boards_no_cards(app):
     new_board = Board(
+        title="Winter", owner= "Lili"
+    )
+    new_board_2 = Board(
         title="Spring", owner="Adriana"
     )
     db.session.add(new_board)
